b6a444270e039c1be4d2b71f167b13e00f7e171e
jvcataquiz <jvcataquiz@gmail.com>
2026-07-16 11:53:57 +0800

    [add] additional creddentials

--- script/resume.js
+++ script/resume.js
@@ -4,7 +4,7 @@ $('#myModal').on('show.bs.modal', function (event) {
   var modal = $(this)
 
   const data = {
-    "Cert1" :`<iframe src="https://drive.google.com/file/d/1qvr266Pj_Gc5mtVME4Jk93b7-F3DZF2R/preview" width="100%" height="600vh" allow="autoplay"></iframe>`,
+    "Cert1" :`<iframe src="https://drive.google.com/file/d/1QKsFraKQjFUxB1kzl_xMSInov-pUP_Q8/preview" width="100%" height="600vh" allow="autoplay"></iframe>`,
     "N5" :`<iframe src="https://drive.google.com/file/d/1FEOwNh9wj4LgpOZ5Ub_iw8kT3ZLVBNOg/preview" width="100%" height="600vh" allow="autoplay"></iframe>`,
     "S6" :`<iframe src="https://drive.google.com/file/d/1mZCRFkZqYoDTRQno2u9aCnZxyG7Kcnrd/preview" width="100%" height="600vh" allow="autoplay"></iframe>`,
     "J01" :`<iframe src="https://drive.google.com/file/d/1COTTK6OMBZ1FF9TQWOjL4S38QCssCDaP/preview" width="100%" height="600vh" allow="autoplay"></iframe>`,
